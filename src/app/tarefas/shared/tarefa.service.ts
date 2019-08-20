3c6b5c41a585c37b82bfaf4468b59c0da49ba270
Bruno Vinicius Felix Da Silva <brunoviniciustica@gmail.com>
2019-08-20 10:27:59 -0300

    Commit: Service

--- src/app/tarefas/shared/tarefa.service.ts
+++ src/app/tarefas/shared/tarefa.service.ts
@@ -14,7 +14,7 @@ export class TarefaService {
     return tarefas  ? JSON.parse(tarefas) : [];
   }
 
-  cadastrar(tarefa: Tarefa): void{
+  cadastrar(tarefa: Tarefa): void {
     const tarefas = this.listarTodos();
     tarefa.id = new Date().getTime();
     tarefas.push(tarefa);
@@ -24,5 +24,32 @@ export class TarefaService {
   buscarPorId(id: number): Tarefa {
     const tarefas: Tarefa[] = this.listarTodos();
     return tarefas.find(tarefa => tarefa.id === id);
+  }
+
+  atualizar(tarefa: Tarefa): void {
+    const tarefas: Tarefa[] = this.listarTodos();
+    tarefas.forEach((obj, index, objs) => {
+      if (tarefa.id === obj.id) {
+        objs[index] = tarefa;
+      }
+    });
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  remover(id: number): void {
+    let tarefas: Tarefa[] = this.listarTodos();
+    tarefas = tarefas.filter(tarefa => tarefa.id !== id);
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  alterarStatus(id: number): void {
+    const tarefas: Tarefa[] = this.listarTodos();
+    tarefas.forEach((obj, index, objs) => {
+      if (id === obj.id) {
+        objs[index].concluido = !obj.concluido;
+      }
+    });
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+
   }
 }
